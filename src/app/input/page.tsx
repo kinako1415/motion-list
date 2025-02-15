6090e1c3ff2a409c4781f98bfe18b4f6177f93ba
kinako1415 <Owata09@outlook.jp>
2025-02-15 17:05:57 +0900

    ModeButtonの作成

--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { GradationButton } from "@/components/GradationButton";
+import { IconButton } from "@/components/IconButton";
 import { Input } from "@/components/Input";
 import { SubmitHandler, useForm } from "react-hook-form";
 
@@ -11,9 +13,64 @@ export default function PageInput() {
 
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
+  const page: string = "top";
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Input label="name" register={register} fieldName="name" />
-    </form>
+    <div style={{ display: "flex", gap: "40px" }}>
+      <form
+        style={{ display: "flex", flexDirection: "column", width: "30%" }}
+        // onSubmit={handleSubmit(onSubmit)}
+      >
+        <Input label="name" register={register} fieldName="name" />
+        <Input
+          label="e-mail address"
+          register={register}
+          fieldName="mailAddress"
+        />
+        <Input label="password" register={register} fieldName="password" />
+        <GradationButton color="red" onclick={handleSubmit(onSubmit)}>
+          作成
+        </GradationButton>
+      </form>
+      <div
+        style={{
+          display: "flex",
+          gap: "10px",
+          boxShadow: "0px 0px 16px rgba(0, 0, 0, 0.1)",
+          backgroundColor: "#ffffff",
+          height: "fit-content",
+          borderRadius: "50px",
+          padding: "8px 16px",
+        }}
+      >
+        {page === "top" ? (
+          <IconButton
+            color="red"
+            url="https://api.iconify.design/heroicons:fire.svg?color=%23ffffff"
+          />
+        ) : (
+          <IconButton
+            color="gray"
+            url="https://api.iconify.design/heroicons:fire.svg?color=%231b2838"
+          />
+        )}
+        {page === "map" ? (
+          <IconButton
+            color="red"
+            url="https://api.iconify.design/material-symbols:photo-album-outline-rounded.svg?color=%23ffffff"
+          />
+        ) : (
+          <IconButton url="https://api.iconify.design/material-symbols:photo-album-outline-rounded.svg?color=%231b2838" />
+        )}
+        {page === "ranking" ? (
+          <IconButton
+            color="red"
+            url="https://api.iconify.design/icon-park-outline:ranking.svg?color=%23ffffff"
+          />
+        ) : (
+          <IconButton url="https://api.iconify.design/icon-park-outline:ranking.svg?color=%231b2838" />
+        )}
+      </div>
+    </div>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -10,7 +10,7 @@ export const Input = ({ label, register, fieldName }: InputProps) => {
   return (
     <div className={styles.inputBox}>
       <label className={styles.label}>{label}</label>
-      <input className={styles.input} {...register(fieldName)} />;
+      <input className={styles.input} {...register(fieldName)} />
     </div>
   );
 };
